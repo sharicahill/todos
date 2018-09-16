aa6bb2f7cdf80b69b9dcde5713d0927d26170ca2
Shari Cahill <sharicahill@gmail.com>
2018-09-16 18:48:54 +1000

    Move database model to api for client & server access

--- client/main.js
+++ client/main.js
@@ -1,41 +1,43 @@
 import './main.html';
 
-Todos = new Mongo.Collection('todos');
+import { Template } from 'meteor/templating';
 
-Todos._collection.insert({text:"Start Meteor project",createdAt:new Date()});
-Todos._collection.insert({text:"Call mum",createdAt:new Date()});
-Todos._collection.insert({text:"Grocery shopping",createdAt:new Date()});
+import { Todos } from '../imports/api/todos.js';
 
-if (Meteor.isClient) {
-  // Template Helpers
-  Template.main.helpers({
-    todos: function(){
-      return Todos.find({}, {sort: {createdAt: -1}});
+// Template Helpers
+Template.main.helpers({
+  todos: function(){
+    return Todos.find({}, {sort: {createdAt: -1}});
+  }
+});
+
+Template.main.events({
+  "submit .new-todo": function(event){
+    var text = event.target.text.value;
+
+    Todos.insert({
+      text: text,
+      createdAt: new Date(),
+      userId: Meteor.userId(),
+      username: Meteor.user().username
+    });
+
+    // Clear Form
+    event.target.text.value='';
+
+    // Prevent Submit
+    return false;
+  },
+  "click .toggle-checked": function(){
+    Todos.update(this._id, {$set:{checked: ! this.checked}});
+  },
+  "click .delete-todo": function(){
+    if(confirm('Are you sure?')){
+      Todos.remove(this._id);
     }
-  });
+  }
+});
 
-  Template.main.events({
-    "submit .new-todo": function(event){
-      var text = event.target.text.value;
-
-      Todos._collection.insert({
-        text: text,
-        createdAt: new Date()
-      });
-
-      // Clear Form
-      event.target.text.value='';
-
-      // Prevent Submit
-      return false;
-    },
-    "click .toggle-checked": function(){
-      Todos._collection.update(this._id, {$set:{checked: ! this.checked}});
-    },
-    "click .delete-todo": function(){
-      if(confirm('Are you sure?')){
-        Todos._collection.remove(this._id);
-      }
-    }
-  });
-}
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_ONLY"
+})
